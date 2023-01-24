fdec3099b49ea358f8afac98a641a06a34abb3bc
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-24 14:14:29 +0100

    testingPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,23 @@ ausgabe in Html
 */
 
 
+const text = "Sam is good at codingschool";
+
+let bad = "bad";
+console.log(bad);
+let school = "school";
+console.log(school);
+let tennis = "tennis";
+console.log(tennis);
+
+let neuMitBad = text.replace("good", bad);
+console.log(neuMitBad);
+document.write(neuMitBad + " "+ "</br>");
+
+let neuMitSchool = text.replace("codingschool", school);
+console.log(neuMitSchool);
+document.write(neuMitSchool + " "+ "</br>");
+
+let neuMitTennis = text.replace("codingschool", tennis);
+console.log(neuMitTennis);
+document.write(neuMitTennis + " " + "</br>");
