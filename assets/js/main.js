5799e94a0d0a44e877416960a891b6412ac279fa
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-24 14:05:59 +0100

    frage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 console.log("test?");
+
+/* 
+Frage
+
+replace
+
+const text = "Sam is good at codingschool";
+
+alles in Variablen speichern
+
+ausgabe in Html
+    1. Sam is bad at school
+    2. Sam is good at school
+    3. Sam is good at tennis
+
+*/
+
+
